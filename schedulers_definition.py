9d0cae5c849bdeb08c5113f9f14990984437b667
Vlad Bogolin <vladbogolin@yahoo.com>
2022-07-19 23:43:23 +0300

    Move non standard builders to a separate master
    The non standard builders (e.g. MSAN, valgrind, eco, etc) need to be manually
    defined. Move them to a separate master so that the rest of the builders
    can be defined automatically based on the os_info.yaml file content.

---
+++ schedulers_definition.py
@@ -0,0 +1,40 @@
+from buildbot.plugins import *
+
+from utils import *
+
+def getSchedulers():
+    l = []
+
+    l.append(schedulers.Triggerable(name="s_upstream_all",
+        builderNames=getBranchBuilderNames))
+
+    schedulerProtectedBranches = schedulers.Triggerable(name="s_protected_branches",
+        builderNames=github_status_builders)
+    l.append(schedulerProtectedBranches)
+
+
+    schedulerPackages = schedulers.Triggerable(name="s_packages",
+            builderNames=getAutobakeBuilderNames)
+    l.append(schedulerPackages)
+
+    schedulerBigtests = schedulers.Triggerable(name="s_bigtest",
+            builderNames=getBigtestBuilderNames)
+    l.append(schedulerBigtests)
+
+    schedulerInstall = schedulers.Triggerable(name="s_install",
+            builderNames=getInstallBuilderNames)
+    l.append(schedulerInstall)
+
+    schedulerUpgrade = schedulers.Triggerable(name="s_upgrade",
+            builderNames=getUpgradeBuilderNames)
+    l.append(schedulerUpgrade)
+
+    schedulerEco = schedulers.Triggerable(name="s_eco",
+            builderNames=getEcoBuilderNames)
+    l.append(schedulerEco)
+
+    schedulerDockerlibrary = schedulers.Triggerable(name="s_dockerlibrary",
+            builderNames=getDockerLibraryNames)
+    l.append(schedulerDockerlibrary)
+
+    return l
