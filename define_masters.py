fe347c4012ff36e687c659e4abbdc629c5e36568
Vlad Bogolin <vladbogolin@yahoo.com>
2022-07-20 23:52:07 +0300

    Auto-generate master configuration
    Auto-generate the master configuration based on the os_info.yaml data
    and split the builders along multiple master processes.
    
    Steps to generate the config:
    1. python define_masters.py
    
    This step creates a series of folders in ./autogen/* each corresponding
    to a different master process.

---
+++ define_masters.py
@@ -0,0 +1,54 @@
+import yaml
+import os
+import shutil
+
+base_path = "autogen/"
+config = { "private": { } }
+exec(open("master-private.cfg").read(), config, { })
+
+with open('os_info.yaml', 'r') as f:
+    os_info = yaml.safe_load(f)
+
+platforms = {}
+
+for os_name in os_info:
+    for arch in os_info[os_name]['arch']:
+        builder_name = arch + '-' + os_name
+        if arch not in platforms:
+            platforms[arch] = []
+        platforms[arch].append(builder_name)
+
+# Clear old configurations
+if os.path.exists(base_path):
+    shutil.rmtree(base_path)
+
+idx = 0
+for arch in platforms:
+    # Create the directory for the architecture that is handled by each master
+    # If for a given architecture there are more than "max_builds" builds,
+    # create multiple masters
+    # "max_builds" is defined is master-private.py
+    num_masters = int(len(platforms[arch]) / config['private']['master-variables']['max_builds']) + 1
+
+    for master_id in range(num_masters):
+        dir_path = base_path + arch + "-master-" + str(master_id)
+        os.makedirs(dir_path)
+
+        master_config = {}
+        master_config['builders'] = platforms[arch]
+        master_config['workers'] = config['private']['master-variables']['workers'][arch]
+        master_config['port'] = config['private']['master-variables']['starting_port'] + idx
+        master_config['log_name'] = "master-docker-" + arch + "-" + str(master_id) + '.log'
+
+        with open(dir_path + '/master-config.yaml', 'w') as f:
+            yaml.dump(master_config, f)
+
+        shutil.copyfile('master.cfg', dir_path + '/master.cfg')
+        shutil.copyfile('master-private.cfg', dir_path + '/master-private.cfg')
+
+        buildbot_tac = open("buildbot.tac", "r").read() % master_config['log_name']
+        with open(dir_path + '/buildbot.tac', 'w') as f:
+            f.write(buildbot_tac)
+        idx += 1
+    print(arch, len(platforms[arch]))
+ 
