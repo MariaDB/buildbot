340262a644aed56eba7323ec3b63d4e595c308a1
Vlad Bogolin <vlad@mariadb.org>
2022-07-29 18:58:36 +0000

    Refine locks for workers
    Refine the lock mechanism to ensure that machines are not overloaded

--- locks.py
+++ locks.py
@@ -9,20 +9,22 @@ from twisted.internet import defer
 from constants import *
 
 ####### LOCKS
-main_master_lock = util.MasterLock('main_master_lock', maxCount=30)
+#main_master_lock = util.MasterLock('main_master_lock', maxCount=30)
 
 hz_bbw1_lock = util.MasterLock('hz_bbw1_lock', maxCount=9)
 hz_bbw2_lock = util.MasterLock('hz_bbw2_lock', maxCount=9)
 hz_bbw4_lock = util.MasterLock('hz_bbw4_lock', maxCount=9)
 hz_bbw5_lock = util.MasterLock('hz_bbw5_lock', maxCount=9)
-intel_bbw1_lock = util.MasterLock('intel_bbw1_lock', maxCount=18)
-p9_rhel8_bbw1_lock = util.MasterLock('p9_rhel8_bbw1_lock', maxCount=4)
-p9_rhel7_bbw1_lock = util.MasterLock('p9_rhel7_bbw1_lock', maxCount=2)
-p9_db_bbw1_lock = util.MasterLock('p9_db_bbw1_lock', maxCount=5)
-aarch_bbw1_lock = util.MasterLock('aarch64_bbw1_lock', maxCount=1)
-aarch_bbw2_lock = util.MasterLock('aarch64_bbw2_lock', maxCount=1)
-aarch_bbw3_lock = util.MasterLock('aarch64_bbw3_lock', maxCount=1)
-aarch_bbw4_lock = util.MasterLock('aarch64_bbw4_lock', maxCount=1)
+amd_bbw1_lock = util.MasterLock('amd_bbw1_lock', maxCount=9)
+amd_bbw2_lock = util.MasterLock('amd_bbw2_lock', maxCount=10)
+intel_bbw1_lock = util.MasterLock('intel_bbw1_lock', maxCount=10)
+p9_rhel8_bbw1_lock = util.MasterLock('p9_rhel8_bbw1_lock', maxCount=9)
+p9_rhel7_bbw1_lock = util.MasterLock('p9_rhel7_bbw1_lock', maxCount=6)
+p9_db_bbw1_lock = util.MasterLock('p9_db_bbw1_lock', maxCount=12)
+aarch_bbw1_lock = util.MasterLock('aarch64_bbw1_lock', maxCount=2)
+aarch_bbw2_lock = util.MasterLock('aarch64_bbw2_lock', maxCount=2)
+aarch_bbw3_lock = util.MasterLock('aarch64_bbw3_lock', maxCount=2)
+aarch_bbw4_lock = util.MasterLock('aarch64_bbw4_lock', maxCount=2)
 aarch_bbw5_lock = util.MasterLock('aarch64_bbw5_lock', maxCount=15)
 apexis_bbw1_lock = util.MasterLock('apexis_bbw1_lock', maxCount=1)
 apexis_bbw2_lock = util.MasterLock('apexis_bbw2_lock', maxCount=1)
@@ -32,8 +34,9 @@ bg_bbw3_lock = util.MasterLock('bg_bbw3_lock', maxCount=2)
 bg_bbw4_lock = util.MasterLock('bg_bbw4_lock', maxCount=2)
 win_bbw1_lock = util.MasterLock('win_bbw1_lock', maxCount=1)
 win_bbw2_lock = util.MasterLock('win_bbw2_lock', maxCount=4)
-s390x_bbw1_lock = util.MasterLock('s390x_bbw1_lock', maxCount=1)
-s390x_bbw2_sles_lock = util.MasterLock('s390x_bbw2_sles_lock', maxCount=1)
+s390x_bbw1_lock = util.MasterLock('s390x_bbw1_lock', maxCount=2)
+s390x_bbw2_lock = util.MasterLock('s390x_bbw2_lock', maxCount=2)
+s390x_bbw3_lock = util.MasterLock('s390x_bbw3_lock', maxCount=2)
 
 @util.renderer
 def getLocks(props):
@@ -42,10 +45,11 @@ def getLocks(props):
     assert worker_name is not None
     assert builder_name is not None
 
-    if builder_name in github_status_builders or builder_name in builders_install or builder_name in builders_upgrade or builder_name in release_builders:
+    if builder_name in github_status_builders or builder_name in builders_install or builder_name in builders_upgrade:
         return []
-    else:
-        locks = [main_master_lock.access('counting')]
+    locks = []
+    #else:
+    #    locks = [main_master_lock.access('counting')]
 
     if 'hz-bbw1-docker' in worker_name:
         locks = locks + [hz_bbw1_lock.access('counting')]
@@ -77,13 +81,13 @@ def getLocks(props):
         locks = locks + [apexis_bbw1_lock.access('counting')]
     if 'fjord2-docker' in worker_name:
         locks = locks + [apexis_bbw2_lock.access('counting')]
-    if 'bg-bbw1-docker' in worker_name:
+    if 'ns-x64-bbw1-docker' in worker_name:
         locks = locks + [bg_bbw1_lock.access('counting')]
-    if 'bg-bbw2-docker' in worker_name:
+    if 'ns-x64--bbw2-docker' in worker_name:
         locks = locks + [bg_bbw2_lock.access('counting')]
-    if 'bg-bbw3-docker' in worker_name:
+    if 'ns-x64--bbw3-docker' in worker_name:
         locks = locks + [bg_bbw3_lock.access('counting')]
-    if 'bg-bbw4-docker' in worker_name:
+    if 'ns-x64-bbw4-docker' in worker_name:
         locks = locks + [bg_bbw4_lock.access('counting')]
     if 'bbw1-docker-windows' in worker_name:
         locks = locks + [win_bbw1_lock.access('counting')]
@@ -92,7 +96,13 @@ def getLocks(props):
     if 's390x-bbw1-docker' in worker_name:
         locks = locks + [s390x_bbw1_lock.access('counting')]
     if 's390x-bbw2-docker' in worker_name:
-        locks = locks + [s390x_bbw2_sles_lock.access('counting')]
+        locks = locks + [s390x_bbw2_lock.access('counting')]
+    if 's390x-bbw3-docker' in worker_name:
+        locks = locks + [s390x_bbw3_lock.access('counting')]
+    if 'amd-bbw1-docker' in worker_name:
+        locks = locks + [amd_bbw1_lock.access('counting')]
+    if 'amd-bbw2-docker' in worker_name:
+        locks = locks + [amd_bbw2_lock.access('counting')]
 
     return locks
 
