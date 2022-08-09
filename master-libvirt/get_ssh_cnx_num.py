d0fd31048a77bf8185dfe798825f3986cdf626b1
Vlad Bogolin <vladbogolin@yahoo.com>
2022-08-09 15:43:15 +0300

    Do not add install/upgrade builds for unreleased OS
    Add a new flag to os_info.yaml that indicates that the addition on
    install/upgrade builds should be skipped or not. This needs to be used
    for unreleased operating systems.

--- master-libvirt/get_ssh_cnx_num.py
+++ master-libvirt/get_ssh_cnx_num.py
@@ -8,7 +8,10 @@ with open('../os_info.yaml', 'r') as f:
 SSH_CONNECTIONS = 0
 for os in os_info:
     for arch in os_info[os]['arch']:
-        if arch not in ['s390x', 'x86'] and os not in ['opensuse-15', 'rhel-9', 'sles-12', 'sles-15']:
+        addInstall = True
+        if 'has_install' in os_info[os]:
+            addInstall = os_info[os]['has_install']
+        if arch not in ['s390x', 'x86'] and addInstall:
             SSH_CONNECTIONS += 1
 
 print(SSH_CONNECTIONS)
