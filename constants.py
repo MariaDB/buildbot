b602c0b06bb6f67f066ad9d83b4ec10af216126e
Vlad Bogolin <vladbogolin@yahoo.com>
2022-08-09 15:43:15 +0300

    Do not add install/upgrade builds for unreleased OS
    Add a new flag to os_info.yaml that indicates that the addition on
    install/upgrade builds should be skipped or not. This needs to be used
    for unreleased operating systems.

--- constants.py
+++ constants.py
@@ -69,8 +69,10 @@ for os in os_info:
         builder_name_autobake = arch + '-' + os + '-' + os_info[os]['type'] + '-autobake'
         builders_autobake.append(builder_name_autobake)
         # Currently there are no VMs for x86 and s390x and OpenSUSE and SLES
-        # RHEL-9 is unreleased, so until packages are available skip install/upgrade
-        if arch not in ['s390x', 'x86'] and os not in ['opensuse-15', 'rhel-9', 'sles-12', 'sles-15']:
+        addInstall = True
+        if 'has_install' in os_info[os]:
+            addInstall = os_info[os]['has_install']
+        if arch not in ['s390x', 'x86'] and addInstall:
             builders_install.append(builder_name_autobake + '-install')
             builders_upgrade.append(builder_name_autobake + '-minor-upgrade')
             builders_upgrade.append(builder_name_autobake + '-major-upgrade')
