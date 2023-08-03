2654efb4ab4d78fbeb15c08e9780458aa9b9ba9e
Faustin Lammler <faustin@fala.red>
2023-08-03 16:29:25 +0200

    Restart work on docker-compose for BBM

--- docker-compose/start-bbm-web.sh
+++ docker-compose/start-bbm-web.sh
@@ -6,21 +6,9 @@ set -o pipefail
 set -o posix
 
 err() {
-  echo_red >&2 "ERROR: $*"
+  echo >&2 "ERROR: $*"
   exit 1
 }
-
-cd /srv/buildbot/master || err "cd /srv/buildbot/master"
-ln -sf /srv/buildbot-config/master-private.cfg master-private.cfg
-
-# config buildbot master-web
-cd /srv/buildbot/master/master-web || err "cd"
-for file in /srv/buildbot-config/master-web/*; do
-  # shellcheck disable=SC2226
-  ln -sf "$file"
-done
-# # loop for debug
-# while true; do date && sleep 30; done
 
 echo "Waiting for MariaDB to start..."
 while ! nc -z mariadb 3306; do
@@ -32,6 +20,11 @@ echo "Waiting for Crossbar to start..."
 while ! nc -z crossbar 8080; do
   sleep 0.1
 done
-echo "Crossbar started"
+echo "MariaDB started"
 
+# # loop for debug
+# while true; do date && sleep 30; done
+
+cd /srv/buildbot/master/master-web || err "cd /srv/buildbot/master/master-web"
+buildbot upgrade-master /srv/buildbot/master/master-web
 buildbot start --nodaemon
