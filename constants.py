9ccbe8712fb60aacd81a84d2a8a0f6f991e48d8f
Daniel Black <daniel@mariadb.org>
2023-09-21 15:45:45 +1000

    Add to branches_main
    To keep non-latent master changes to a minium we plan for 4 more
    11.x series releases.
    
    This way non-latent workers will pick up jobs as these new branches
    becomes active (every 3 months).

--- constants.py
+++ constants.py
@@ -14,6 +14,11 @@ branches_main = [
         '11.1',
         '11.2',
         '11.3',
+        '11.4',
+        '11.5',
+        '11.6',
+        '11.7',
+        '11.8',
         ]
 
 # Defines what builders report status to GitHub
