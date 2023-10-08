722542e87963d275a4dffc75c35f7b84c5dac396
Faustin Lammler <faustin@fala.red>
2023-10-08 13:51:19 +0200

    Workflow dependency

--- utils.py
+++ utils.py
@@ -111,7 +111,7 @@ def downloadSourceTarball(output_dir="/mnt/packages/"):
     if [[ $os != "AIX" && $os != "Darwin" ]]; then
       use_flock="flock \"$d$f\" "
     fi
-    cmd="$use_flock wget -cO \"$d$f\" \"https://ci.mariadb.org/%(prop:tarbuildnum)s/%(prop:mariadb_version)s.tar.gz\""
+    cmd="$use_flock wget -cO \"$d$f\" \"""" + os.getenv('ARTIFACTS_URL', default='https://ci.mariadb.org') + """/%(prop:tarbuildnum)s/%(prop:mariadb_version)s.tar.gz\""
 
     res=1
     for i in {1..10}; do
