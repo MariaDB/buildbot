9cf023d14960c877cffd60284ceed3b2e2d56a75
Faustin Lammler <faustin@fala.red>
2024-03-12 18:55:31 +0100

    Bash linting

--- script_templates/bintar_providers.sh
+++ script_templates/bintar_providers.sh
@@ -2,15 +2,19 @@
 
 set -ex
 
+err() {
+  echo >&2 "ERROR: $*"
+  exit 1
+}
+
 buildername="%(prop:buildername)s"
 
-cd buildbot/build
+cd buildbot/build || err "cd buildbot/build"
 num_providers_expected=$(find plugin/provider* | wc -l)
 if ((num_providers_expected == 0)); then
-  echo "No expected providers found, skipping the test"
-  exit
+  err "No expected providers found, skipping the test"
 fi
-if [[ "$buildername" == "kvm-rpm-rhel8-ppc64le" ]]; then
+if [[ $buildername == "kvm-rpm-rhel8-ppc64le" ]]; then
   echo "Test warning"": This builder cannot build all providers due to MDEV-28738"
   num_providers_expected=2
 fi
@@ -18,7 +22,6 @@ fi
 find mkbin/plugin/ -name "provider*.so"
 num_providers_built=$(find mkbin/plugin/ -name "provider*.so" | wc -l)
 
-if [[ "$num_providers_built" != "$num_providers_expected" ]]; then
-  echo "ERROR: Found $num_providers_built provider libraries, expected $num_providers_expected"
-  exit 1
+if ((num_providers_built != num_providers_expected)); then
+  err "ERROR: Found $num_providers_built provider libraries, expected $num_providers_expected"
 fi
