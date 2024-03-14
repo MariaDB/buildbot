8dbccc4fca765e6f017d1dedbc96237308b4651b
Faustin Lammler <faustin@fala.red>
2024-03-14 16:40:48 +0100

    Fix shell script

--- script_templates/bintar_providers.sh
+++ script_templates/bintar_providers.sh
@@ -10,12 +10,13 @@ err() {
 buildername="%(prop:buildername)s"
 
 cd buildbot/build || err "cd buildbot/build"
-num_providers_expected=$(find plugin/provider* | wc -l)
+# shellcheck disable=SC2010
+num_providers_expected=$(ls plugin/* | grep -cE "^provider")
 if ((num_providers_expected == 0)); then
   err "No expected providers found, skipping the test"
 fi
 if [[ $buildername == "kvm-rpm-rhel8-ppc64le" ]]; then
-  echo "Test warning"": This builder cannot build all providers due to MDEV-28738"
+  echo "Test warning: This builder cannot build all providers due to MDEV-28738"
   num_providers_expected=2
 fi
 
