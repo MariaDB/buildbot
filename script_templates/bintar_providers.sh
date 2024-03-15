d638fb6deff7306027fe9d5cd74ad1fb08bbe02e
Faustin Lammler <faustin@fala.red>
2024-03-15 11:30:23 +0100

    Wrong exit code

--- script_templates/bintar_providers.sh
+++ script_templates/bintar_providers.sh
@@ -13,7 +13,8 @@ cd buildbot/build || err "cd buildbot/build"
 # shellcheck disable=SC2010
 num_providers_expected=$(ls plugin/* | grep -cE "^provider")
 if ((num_providers_expected == 0)); then
-  err "No expected providers found, skipping the test"
+  echo "No expected providers found, skipping the test"
+  exit 0
 fi
 if [[ $buildername == "kvm-rpm-rhel8-ppc64le" ]]; then
   echo "Test warning: This builder cannot build all providers due to MDEV-28738"
