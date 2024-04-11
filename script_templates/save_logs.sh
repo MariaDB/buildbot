527d9d594371df17321865822f855f1876c17fd8
Faustin Lammler <faustin@fala.red>
2024-04-11 15:02:12 +0200

    Fix save_logs for FreeBSD nonlatent worker

--- script_templates/save_logs.sh
+++ script_templates/save_logs.sh
@@ -13,12 +13,25 @@ artifacts_url=${ARTIFACTS_URL:-https://ci.mariadb.org}
 typeset -r tarbuildnum="%(prop:tarbuildnum)s"
 typeset -r buildername="%(prop:buildername)s"
 
+if [[ $artifacts_url == "https://ci.dev.mariadb.org" ]]; then
+  artifact_dest_extra="dev_"
+else
+  artifact_dest_extra=""
+fi
+
+os=$(uname -s)
+if [[ $os == "FreeBSD" ]]; then
+  typeset artifact_dest="/mnt/autofs/master_${artifact_dest_extra}packages"
+else
+  typeset artifact_dest="/packages"
+fi
+
 [[ -d ./buildbot/logs ]] ||
   err "./buildbot/logs does not exist, no log can be saved"
 
-[[ -d /packages/$tarbuildnum/logs/$buildername/ ]] ||
-  mkdir -p /packages/$tarbuildnum/logs/$buildername
+[[ -d $artifact_dest/$tarbuildnum/logs/$buildername/ ]] ||
+  mkdir -p $artifact_dest/$tarbuildnum/logs/$buildername
 
-cp -rv ./buildbot/logs/* /packages/$tarbuildnum/logs/$buildername/
+cp -rv ./buildbot/logs/* "$artifact_dest/$tarbuildnum/logs/$buildername/"
 
 echo "Logs available at $artifacts_url/$tarbuildnum/logs/$buildername/"
