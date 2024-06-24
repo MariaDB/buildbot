bcda293985c3fd4f9d544a05f13dd77550215e2b
Vlad Bogolin <vladbogolin@gmail.com>
2024-06-24 19:59:15 +0300

    Jepsen fixes
    - download step
    - add env vars
    - solve bash issue that makes lein interpret "$LEIN_OPTIONS" as a
    single argument

--- script_templates/jepsen_mysql.sh
+++ script_templates/jepsen_mysql.sh
@@ -9,15 +9,18 @@ err() {
 
 NPROC="%(prop:jobs)s"
 
-LEIN_OPTIONS="run test \
-  --db maria-docker \
-  --nodes localhost \
-  --concurrency ${NPROC} \
-  --rate 1000 \
-  --time-limit 60 \
-  --key-count 40 \
-  --no-ssh=true \
-  --innodb-strict-isolation=true"
+LEIN_OPTIONS=(
+  "run"
+  "test"
+  "--db" "maria-docker"
+  "--nodes" "localhost"
+  "--concurrency ${NPROC}"
+  "--rate" "1000"
+  "--time-limit" "60"
+  "--key-count" "40"
+  "--no-ssh=true"
+  "--innodb-strict-isolation=true"
+)
 
 cd ../jepsen-mariadb ||
   err "cd ../jepsen-mariadb"
@@ -28,25 +31,25 @@ log_lines() {
   echo -e "\n${line}\n${msg}\n${line}\n"
 }
 log_lines "Append serializable"
-../lein "$LEIN_OPTIONS" -w append -i serializable
+../lein "${LEIN_OPTIONS[@]}" -w append -i serializable
 
 log_lines "Append repeatable-read"
-../lein "$LEIN_OPTIONS" -w append -i repeatable-read
+../lein "${LEIN_OPTIONS[@]}" -w append -i repeatable-read
 
 log_lines "Append read-committed"
-../lein "$LEIN_OPTIONS" -w append -i read-committed
+../lein "${LEIN_OPTIONS[@]}" -w append -i read-committed
 
 log_lines "Append read-uncommitted"
-../lein "$LEIN_OPTIONS" -w append -i read-uncommitted
+../lein "${LEIN_OPTIONS[@]}" -w append -i read-uncommitted
 
 log_lines "Non-repeatable read serializable"
-../lein "$LEIN_OPTIONS" -w nonrepeatable-read -i serializable
+../lein "${LEIN_OPTIONS[@]}" -w nonrepeatable-read -i serializable
 
 log_lines "Non-repeatable repeatable-read"
-../lein "$LEIN_OPTIONS" -w nonrepeatable-read -i repeatable-read
+../lein "${LEIN_OPTIONS[@]}" -w nonrepeatable-read -i repeatable-read
 
 log_lines "mav serializable"
-../lein "$LEIN_OPTIONS" -w mav -i serializable
+../lein "${LEIN_OPTIONS[@]}" -w mav -i serializable
 
 log_lines "mav repeatable-read"
-../lein "$LEIN_OPTIONS" -w mav -i repeatable-read
+../lein "${LEIN_OPTIONS[@]}" -w mav -i repeatable-read
