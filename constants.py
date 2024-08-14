09a6947b075282b461000660e4c5dd10235090b9
Daniel Black <daniel@mariadb.org>
2024-08-14 11:44:58 +1000

    MDBF-337: galera builds RHEL
    With MariaDB Operator doing UBI based container
    operability with galera, more RHEL based galera
    tests needed to catch operability problems ealier.

--- constants.py
+++ constants.py
@@ -76,11 +76,14 @@ else:
 
 BUILDERS_GALERA_MTR = [
     "aarch64-debian-12",
+    "aarch64-rhel-9",
     "amd64-freebsd-14",
+    "amd64-rhel-9",
     "amd64-ubuntu-2404-debug",
     "s390x-ubuntu-2204",
     "ppc64le-ubuntu-2204",
     "ppc64le-ubuntu-2404",
+    "ppc64le-rhel-9",
 ]
 BUILDERS_S3_MTR = [
     "aarch64-ubuntu-2204-debug",
