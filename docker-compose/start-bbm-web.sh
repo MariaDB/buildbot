556d12255521ae5b12baf3973472724ea4b57c3e
Razvan-Liviu Varzaru <razvan@mariadb.org>
2024-10-15 17:26:16 +0300

    Switch BBM deployment to Host Networking

--- docker-compose/start-bbm-web.sh
+++ docker-compose/start-bbm-web.sh
@@ -10,23 +10,24 @@ err() {
   exit 1
 }
 
+cd /srv/buildbot/master/master-web || err "cd /srv/buildbot/master/master-web"
+
+# # loop for debug
+# while true; do date && sleep 30; done
+
+# shellcheck disable=SC2226
+[[ -f master-private.cfg ]] || ln -s ../master-private.cfg
+VAR_DB_HOST=$(grep db_host master-private.cfg | awk '{print $3}' | sed s/\"//g)
 echo "Waiting for MariaDB to start..."
-while ! nc -z mariadb 3306; do
+while ! nc -z $VAR_DB_HOST 3306; do
   sleep 0.1
 done
 echo "MariaDB started"
 
 echo "Waiting for Crossbar to start..."
-while ! nc -z crossbar 8080; do
+while ! nc -z 127.0.0.1 8080; do
   sleep 0.1
 done
-echo "MariaDB started"
-
-# # loop for debug
-# while true; do date && sleep 30; done
-
-cd /srv/buildbot/master/master-web || err "cd /srv/buildbot/master/master-web"
-# shellcheck disable=SC2226
-[[ -f master-private.cfg ]] || ln -s ../master-private.cfg
+echo "Crossbar started"
 buildbot upgrade-master /srv/buildbot/master/master-web
 buildbot start --nodaemon
