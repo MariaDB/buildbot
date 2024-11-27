e423a57d60265352bfe58ded6d1c77c1e22cd2ec
Razvan-Liviu Varzaru <razvan@mariadb.org>
2024-11-27 13:57:57 +0200

    Consider running config validation on both tags
    When master container is under development we must ensure  that configuration changes are valid on both buildbot master versions, PROD/DEV.
    Let's say one will bring a new feature to Production independently of a master upgrade.

--- validate_master_cfg.sh
+++ validate_master_cfg.sh
@@ -10,6 +10,43 @@ err() {
   exit 1
 }
 
+usage() {
+  echo "Usage: $0 -e <DEV|PROD>"
+  exit 1
+}
+
+ENVIRONMENT=""
+
+while getopts ":e:" opt; do
+  case ${opt} in
+    e )
+      ENVIRONMENT=$OPTARG
+      ;;
+    \? )
+      usage
+      ;;
+    : )
+      usage
+      ;;
+  esac
+done
+
+if [[ -z "$ENVIRONMENT" ]]; then
+  usage
+fi
+
+case $ENVIRONMENT in
+  DEV)
+    IMAGE="quay.io/mariadb-foundation/bb-master:dev_master"
+    ;;
+  PROD)
+    IMAGE="quay.io/mariadb-foundation/bb-master:master"
+    ;;
+  *)
+    err "Unknown environment: $ENVIRONMENT. Use DEV or PROD."
+    ;;
+esac
+
 mkdir -p master-credential-provider
 [[ -f master-private.cfg ]] ||
   ln -s master-private.cfg-sample master-private.cfg
@@ -33,7 +70,7 @@ python3 define_masters.py
 echo "Checking master.cfg"
 $RUNC run -i -v "$(pwd):/srv/buildbot/master" \
   -w /srv/buildbot/master \
-  quay.io/mariadb-foundation/bb-master:master \
+  $IMAGE \
   buildbot checkconfig master.cfg
 echo -e "done\n"
 # not checking libvirt config file (//TEMP we need to find a solution
@@ -49,7 +86,7 @@ for dir in autogen/* \
   echo "Checking $dir/master.cfg"
   $RUNC run -i -v "$(pwd):/srv/buildbot/master" \
     -w /srv/buildbot/master \
-    quay.io/mariadb-foundation/bb-master:master \
+    $IMAGE \
     bash -c "cd $dir && buildbot checkconfig master.cfg"
   echo -e "done\n"
 done
