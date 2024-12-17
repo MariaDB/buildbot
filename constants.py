adcf4aee04b4ed4fb43fffa2d27966a0871f59f6
Daniel Black <daniel@mariadb.org>
2024-12-17 12:33:06 +1100

    MDBF-337: add galera mtr tests for x86_32
    Without it could be going out of support
    https://github.com/MariaDB/galera/pull/32#issuecomment-2545358845

--- constants.py
+++ constants.py
@@ -84,6 +84,7 @@ BUILDERS_GALERA_MTR = [
     "ppc64le-ubuntu-2204",
     "ppc64le-ubuntu-2404",
     "ppc64le-rhel-9",
+    "x86-debian-12",
 ]
 BUILDERS_S3_MTR = [
     "aarch64-ubuntu-2204-debug",
