d48d5062cf9221233cf7059e0a87cebb79853463
Daniel Black <daniel@mariadb.org>
2025-01-17 18:12:28 +1100

    MDBF-897 SLES 15.5 EOL

--- constants.py
+++ constants.py
@@ -143,11 +143,9 @@ SUPPORTED_PLATFORMS["10.5"] += [
 SUPPORTED_PLATFORMS["10.6"] += [
     "aarch64-ubuntu-2204",
     "amd64-opensuse-1505",
-    "amd64-sles-1505",
     "amd64-ubuntu-2204",
     "ppc64le-ubuntu-2204",
     "s390x-ubuntu-2204",
-    "s390x-sles-1505",
     "x86-debian-12",
 ]
 
