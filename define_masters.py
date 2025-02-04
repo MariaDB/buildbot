c0e0ce964b4e29f3f3c0cbd0cee6fb4286def734
Vicențiu Ciorbaru <cvicentiu@gmail.com>
2025-02-05 01:30:39 +0200

    cleanup define_masters.py and os_info.yaml
    This is a larger commit that does the following:
    
    1. os_info now specifically mentions the image_tag to use for the OS.
    2. os_info specifies tags per os.
    3. define_masters now reads os_info and groups by arch.
    4. Make autogen buildbot.tac allow relative import
    5. buildbot.tac no longer a "template" with log name filled in by master
       name, but a file that gets copied for each master.
    6. Validate master updated to change the way each master should be
       started.

--- define_masters.py
+++ define_masters.py
@@ -2,26 +2,33 @@
 
 import os
 import shutil
+from collections import defaultdict
 
 import yaml
 
 BASE_PATH = "autogen/"
 config = {"private": {}}
-exec(open("master-private.cfg").read(), config, {})
+with open("master-private.cfg", "r") as file:
+    exec(file.read(), config, {})
+
+master_variables = config["private"]["master-variables"]
 
 with open("os_info.yaml", encoding="utf-8") as file:
     OS_INFO = yaml.safe_load(file)
 
-platforms = {}
+platforms = defaultdict(dict)
 
 for os_name in OS_INFO:
+    # TODO(cvicentiu) this should be removed, hack.
     if "install_only" in OS_INFO[os_name] and OS_INFO[os_name]["install_only"]:
         continue
+
     for arch in OS_INFO[os_name]["arch"]:
-        builder_name = arch + "-" + os_name
-        if arch not in platforms:
-            platforms[arch] = []
-        platforms[arch].append(builder_name)
+        builder_name = f"{arch}-{os_name}"
+        platforms[arch][os_name] = {
+            "image_tag": OS_INFO[os_name]["image_tag"],
+            "tags": OS_INFO[os_name]["tags"],
+        }
 
 # Clear old configurations
 if os.path.exists(BASE_PATH):
@@ -32,33 +39,23 @@ for arch in platforms:
     # If for a given architecture there are more than "max_builds" builds,
     # create multiple masters
     # "max_builds" is defined is master-private.py
-    num_masters = (
-        int(len(platforms[arch]) / config["private"]["master-variables"]["max_builds"])
-        + 1
-    )
+    num_masters = int(len(platforms[arch]) / master_variables["max_builds"]) + 1
 
     for master_id in range(num_masters):
-        dir_path = BASE_PATH + arch + "-master-" + str(master_id)
+        dir_path = f"{BASE_PATH}{arch}-master-{master_id}"
         os.makedirs(dir_path)
 
-        master_config = {}
-        master_config["builders"] = platforms[arch]
-        master_config["workers"] = config["private"]["master-variables"]["workers"][
-            arch
-        ]
-        master_config["log_name"] = (
-            "master-docker-" + arch + "-" + str(master_id) + ".log"
-        )
+        master_config = {
+            "builders": {arch: platforms[arch]},
+            "workers": master_variables["workers"][arch],
+            "log_name": f"master-docker-{arch}-{master_id}.log",
+        }
 
-        with open(dir_path + "/master-config.yaml", mode="w", encoding="utf-8") as file:
+        with open(f"{dir_path}/master-config.yaml", mode="w", encoding="utf-8") as file:
             yaml.dump(master_config, file)
 
         shutil.copyfile("master.cfg", dir_path + "/master.cfg")
         shutil.copyfile("master-private.cfg", dir_path + "/master-private.cfg")
+        shutil.copyfile("buildbot.tac", dir_path + "/buildbot.tac")
 
-        buildbot_tac = (
-            open("buildbot.tac", encoding="utf-8").read() % master_config["log_name"]
-        )
-        with open(dir_path + "/buildbot.tac", mode="w", encoding="utf-8") as f:
-            f.write(buildbot_tac)
     print(arch, len(master_config["builders"]))
