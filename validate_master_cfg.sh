3993fbdc46d4e0778c62b0404d43d0ac5dcfb12e
Razvan-Liviu Varzaru <razvan@mariadb.org>
2025-03-10 15:28:01 +0200

    Validate master-libvirt using a dummy connection

--- validate_master_cfg.sh
+++ validate_master_cfg.sh
@@ -69,13 +69,12 @@ command -v python3 >/dev/null ||
   err "python3 command not found"
 
 python3 define_masters.py
-# not checking libvirt config file (//TEMP we need to find a solution
-# to not check ssh connection)
 for dir in autogen/* \
   master-docker-nonstandard \
   master-docker-nonstandard-2 \
   master-galera \
   master-nonlatent \
+  master-libvirt \
   master-protected-branches \
   master-web; do
   echo "Checking $dir/master.cfg"
