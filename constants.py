cda54d5938b0791db027f2d104777f4b122a7913
Razvan-Liviu Varzaru <razvan@mariadb.org>
2025-06-30 14:43:18 +0300

    MDBF-1067 Add SLES 15 SP7
    + MDEV-34942 removes eigen3 as a dep + clarify judy-devel usage

--- constants.py
+++ constants.py
@@ -202,8 +202,10 @@ SUPPORTED_PLATFORMS["11.4"] = SUPPORTED_PLATFORMS["11.3"].copy()
 SUPPORTED_PLATFORMS["11.4"] += [
     "aarch64-ubuntu-2410",
     "aarch64-ubuntu-2504",
+    "amd64-sles-1507",
     "amd64-ubuntu-2410",
     "amd64-ubuntu-2504",
+    "s390x-sles-1507",
 ]
 
 SUPPORTED_PLATFORMS["11.5"] = SUPPORTED_PLATFORMS["11.4"].copy()
