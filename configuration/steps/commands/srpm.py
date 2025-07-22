97e35c58abd6fc511fc779e6f8b142c1b84b10ae
Razvan-Liviu Varzaru <razvan@mariadb.org>
2025-07-22 11:50:46 +0300

    Define srpm test scripts
    - install build-time deps. rhel needs a valid subscription
    - re-build from the .src.rpm . Use ccache
    - compare the results. First we need to check that we produce the same rpms then we deep-dive and compare per/rpm dependencies
    
    Framework fix:
    - do not use interpolate for load_script. The only way variables can be passed to
    scripts is through command line arguments.

---
+++ configuration/steps/commands/srpm.py
@@ -0,0 +1,40 @@
+from pathlib import PurePath
+
+from configuration.steps.commands.base import BashScriptCommand
+
+
+class SRPMInstallBuildDeps(BashScriptCommand):
+    """
+    A command to install build dependencies from a source RPM (SRPM) file.
+    """
+
+    def __init__(self, workdir: PurePath = PurePath(".")):
+        dir_srpms = "srpms"
+        args = [dir_srpms]
+        super().__init__(
+            script_name="srpm_install_build_deps.sh", workdir=workdir, args=args
+        )
+
+
+class SRPMRebuild(BashScriptCommand):
+    """
+    A command to rebuild the RPM's from a source RPM.
+    """
+
+    def __init__(self, jobs: int, workdir: PurePath = PurePath(".")):
+        dir_srpms = "srpms"
+        args = [dir_srpms, jobs]
+        super().__init__(script_name="srpm_rebuild.sh", workdir=workdir, args=args)
+
+
+class SRPMCompare(BashScriptCommand):
+    """
+    A command to compare the RPMs from the CI and rebuilt directories.
+    """
+
+    def __init__(self, workdir: PurePath = PurePath(".")):
+        ci_rpms_dir = "rpms"
+        rebuilt_rpms_dir = "../rpmbuild/RPMS"
+        exclude_rpms = "MariaDB-compat*"
+        args = [ci_rpms_dir, rebuilt_rpms_dir, exclude_rpms]
+        super().__init__(script_name="srpm_compare.sh", workdir=workdir, args=args)
