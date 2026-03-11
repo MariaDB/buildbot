5f8f88fc4a27a388d386162822d97da7bce4e573
Razvan-Liviu Varzaru <razvan@mariadb.org>
2026-03-11 12:57:21 +0200

    MDBF-1189: No-op for C/C, C/CPP
    Avoid triggering dummy builders until the factories
    are implemented.

--- configuration/builders/definitions/connectors/conc.py
+++ configuration/builders/definitions/connectors/conc.py
@@ -2,16 +2,4 @@ from configuration.builders.base import GenericBuilder
 from configuration.builders.sequences.connectors.conc import deb, rpm, tarball
 
 TARBALL = GenericBuilder(name="cc-tarball-docker", sequences=[tarball()])
-
-AMD64_RPM_BUILDERS = [
-    GenericBuilder(name="cc-amd64-fedora43", sequences=[rpm()]),
-    GenericBuilder(name="cc-amd64-fedora42", sequences=[rpm()]),
-]
-
-AMD64_DEB_BUILDERS = [
-    GenericBuilder(name="cc-amd64-debian12", sequences=[deb()]),
-    GenericBuilder(name="cc-amd64-debian11", sequences=[deb()]),
-]
-RPM_BUILDERS = [*AMD64_RPM_BUILDERS]
-
-DEB_BUILDERS = [*AMD64_DEB_BUILDERS]
+RELEASE_BUILDERS_BY_ARCH = {"amd64": [], "aarch64": []}
